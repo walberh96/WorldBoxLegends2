6499a53d81067323d116fc959ea2738511f30937
walberh96 <walberh96@gmail.com>
2022-12-15 19:29:31 -0500

    Scroll added to levels

--- Assets/Scripts/UIManager.cs
+++ Assets/Scripts/UIManager.cs
@@ -2,10 +2,11 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using TMPro;
 
 public class UIManager : MonoBehaviour
 {
-    public GameObject PanelMainMenu, panelLevelMenu;
+    [SerializeField] GameObject PanelMainMenu, panelLevelMenu,mainCamera,volumeBtn;
     // Start is called before the first frame update
     void Start()
     {
@@ -34,5 +35,11 @@ public class UIManager : MonoBehaviour
     }
     public void StartLevel(int index){
         SceneManager.LoadScene(index);
+    }
+    public void MuteAudio(){
+        AudioSource a = mainCamera.gameObject.GetComponent<AudioSource>();
+        TextMeshProUGUI tmp = volumeBtn.gameObject.GetComponentInChildren<TextMeshProUGUI>();
+        a.volume= a.volume==0 ? 1 : 0;
+        tmp.color = a.volume == 0 ? Color.red : Color.green ;
     }
 }
